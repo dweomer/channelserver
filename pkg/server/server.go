ec56256663caf59573e53a7e000b343bdc14910c
Menna <menna.elmasry@rancher.com>
2020-07-29 22:29:28 +0200

    changes1

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"net/url"
 	"os"
@@ -18,40 +19,47 @@ import (
 	"github.com/rancher/steve/pkg/schemaserver/types"
 )
 
-func ListenAndServe(ctx context.Context, address string, config *config.Config, pathPrefix string) error {
+func ListenAndServe(ctx context.Context, address string, configs []*config.Config, pathPrefix []string) error {
 	server := server.DefaultAPIServer()
-	server.Schemas.MustImportAndCustomize(model.Channel{}, func(schema *types.APISchema) {
-		schema.Store = store.New(config)
-		schema.CollectionMethods = []string{http.MethodGet}
-		schema.ResourceMethods = []string{http.MethodGet}
-	})
-	server.Schemas.MustImportAndCustomize(model.Release{}, func(schema *types.APISchema) {
-		schema.Store = release.New(config)
-		schema.CollectionMethods = []string{http.MethodGet}
-	})
+	var err error
+	for index, config := range configs {
+		server.Schemas.MustImportAndCustomize(model.Channel{}, func(schema *types.APISchema) {
+			schema.Store = store.New(config)
+			schema.CollectionMethods = []string{http.MethodGet}
+			schema.ResourceMethods = []string{http.MethodGet}
+		})
+		server.Schemas.MustImportAndCustomize(model.Release{}, func(schema *types.APISchema) {
+			schema.Store = release.New(config)
+			schema.CollectionMethods = []string{http.MethodGet}
+		})
 
-	pathPrefix = strings.TrimPrefix(pathPrefix, "/")
-	pathPrefix = strings.TrimSuffix(pathPrefix, "/")
+		pathPrefix[index] = strings.TrimPrefix(pathPrefix[index], "/")
+		pathPrefix[index] = strings.TrimSuffix(pathPrefix[index], "/")
 
-	apiroot.Register(server.Schemas, []string{pathPrefix}, nil)
-	router := mux.NewRouter()
-	router.MatcherFunc(setType("apiRoot", pathPrefix)).Path("/").Handler(server)
-	router.MatcherFunc(setType("apiRoot", pathPrefix)).Path("/{name}").Handler(server)
-	router.Path("/{prefix:" + pathPrefix + "}/{type}").Handler(server)
-	router.Path("/{prefix:" + pathPrefix + "}/{type}/{name}").Handler(server)
+		router := mux.NewRouter()
 
-	next := handlers.LoggingHandler(os.Stdout, router)
-	handler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		user := req.Header.Get("X-SUC-Cluster-ID")
-		if user != "" && req.URL != nil {
-			req.URL.User = url.User(user)
-		}
-		next.ServeHTTP(rw, req)
-	})
+		apiroot.Register(server.Schemas, []string{pathPrefix[index]}, nil)
+		router.MatcherFunc(setType("apiRoot", pathPrefix[index])).Path("/").Handler(server)
+		router.MatcherFunc(setType("apiRoot", pathPrefix[index])).Path("/{name}").Handler(server)
+		router.Path("/{prefix:" + pathPrefix[index] + "}/{type}").Handler(server)
+		router.Path("/{prefix:" + pathPrefix[index] + "}/{type}/{name}").Handler(server)
 
-	return http.ListenAndServe(address, handler)
+		fmt.Println(config, pathPrefix[index], config.ChannelsConfig())
+		next := handlers.LoggingHandler(os.Stdout, router)
+		handler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			user := req.Header.Get("X-SUC-Cluster-ID")
+			if user != "" && req.URL != nil {
+				req.URL.User = url.User(user)
+			}
+			next.ServeHTTP(rw, req)
+		})
+
+		err = http.ListenAndServe(address, handler)
+
+	}
+
+	return err
 }
-
 func setType(t string, pathPrefix string) mux.MatcherFunc {
 	return func(request *http.Request, match *mux.RouteMatch) bool {
 		if match.Vars == nil {
